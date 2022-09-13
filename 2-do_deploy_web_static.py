7a25281f050791bb5d2e748213a648f74cce3407
NottSr <leonardo.br4@hotmail.com>
2022-09-13 17:48:13 -0500

    Fixing code, clean up and setting up web_flask

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -32,21 +32,22 @@ def do_deploy(archive_path):
     Returns True if all operations have been done correctly,
     otherwise returns False
     """
-    if not os.path.exists(archive_path):
-        return False
-    try:
-        put(archive_path, "/tmp/")
-        file = archive_path.split('/')[-1].split('.')[0]
-        sudo("mkdir -p /data/web_static/releases/{}/".format(file))
-        sudo("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/"
-             .format(file, file))
-        sudo("rm /tmp/{}.tgz".format(file))
-        sudo("mv /data/web_static/releases/{}/web_static/*\
-        /data/web_static/releases/{}/".format(file, file))
-        sudo("rm -rf /data/web_static/releases/{}/web_static".format(file))
-        sudo("rm -rf /data/web_static/current")
-        sudo("ln -s /data/web_static/releases/{}/ /data/web_static/current"
-             .format(file))
+    if os.path.exists(archive_path):
+        archive = archive_path.split('/')[1]
+        a_path = "/tmp/{}".format(archive)
+        print(a_path)
+        folder = archive.split('.')[0]
+        f_path = "/data/web_static/releases/{}/".format(folder)
+
+        put(archive_path, a_path)
+        run("mkdir -p {}".format(f_path))
+        run("tar -xzf {} -C {}".format(a_path, f_path))
+        run("rm {}".format(a_path))
+        run("mv -f {}web_static/* {}".format(f_path, f_path))
+        run("rm -rf {}web_static".format(f_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(f_path))
+
         return True
-    except Exception:
-        return False
+
+    return False
