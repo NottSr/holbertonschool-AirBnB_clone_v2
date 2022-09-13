a840e2a48ac5d3608125dea353b8b5c4424ef3ce
NottSr <leonardo.br4@hotmail.com>
2022-09-13 18:41:52 -0500

    Task 100 fixed

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -7,30 +7,28 @@ web servers, using the function deploy
 import os
 from fabric.api import *
 
-
 env.hosts = ['34.229.203.168', '34.207.117.5']
 
 
-def do_clean(num=0):
+def do_clean(number=0):
     """
     Delete out-of-date archives.
     Args:
-        num (int): The number of archives to keep.
+        number (int): The number of archives to keep.
 
     If number is 0 or 1, keeps only the most recent archive.
     If number is 2, keeps the most and second-most recent archives,
     etc.
     """
-    num = 1 if int(num) == 0 else int(num)
+    number = 1 if int(number) == 0 else int(number)
 
     archives = sorted(os.listdir("versions"))
-    [archives.pop() for i in range(num)]
-
+    [archives.pop() for i in range(number)]
     with lcd("versions"):
         [local("rm ./{}".format(a)) for a in archives]
 
     with cd("/data/web_static/releases"):
         archives = run("ls -tr").split()
         archives = [a for a in archives if "web_static_" in a]
-        [archives.pop() for i in range(num)]
+        [archives.pop() for i in range(number)]
         [run("rm -rf ./{}".format(a)) for a in archives]
